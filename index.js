6665c7e8c39306b91ccf5ccccc83d1df7ae9550e
ebaquera <edna.baquera@gmail.com>
2021-10-05 17:01:12 -0700

    add a carousel in CSS

---
+++ index.js
@@ -0,0 +1,34 @@
+const slides = document.getElementsByClassName("carousel-item");
+let slidePosition = 0;
+const totalSlides = slides.length;
+
+document
+  .getElementById("carousel-btn-next")
+  .addEventListener("click", function moveToNextSlide() {
+    hideAllSlides();
+    if (slidePosition === totalSlides - 1) {
+      slidePosition = 0;
+    } else {
+      slidePosition += 1;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
+  });
+
+document
+  .getElementById("carousel-btn-prev")
+  .addEventListener("click", function moveToPrevSlide() {
+    hideAllSlides();
+    if (slidePosition === 0) {
+      slidePosition = totalSlides - 1;
+    } else {
+      slidePosition -= 1;
+    }
+    slides[slidePosition].classList.add("carousel-item-visible");
+  });
+
+function hideAllSlides() {
+  for (const slide of slides) {
+    slide.classList.remove("carousel-item-visible");
+    slide.classList.add("carousel-item-hidden");
+  }
+}
